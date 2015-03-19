f1e588886ce0e2ac556dc0b167c1c05711f52e58
Josef Bacik <jbacik@fb.com>
2015-03-19 11:17:11 -0400

    flesh out replay-individual.sh
    Replaying each individual IO is kind of intensive, and is tricky since we also
    want to make sure the log replays properly and such.  So instead of replaying
    up to a new point every time just create a snapshot and mount and test that to
    verify things went ok.
    
    Signed-off-by: Josef Bacik <jbacik@fb.com>

--- replay-individual.sh
+++ replay-individual.sh
@@ -1,19 +1,78 @@
 #!/bin/bash
 
-[ $# -ne 2 ] && echo "Usage: replay-individual.sh <logdev> <replaydev>" && \
-	exit 1
+# Modify these
+LOGDEV=
+REPLAYDEV=
+FSCK=
+TEST_MNT=
+START_MARK="mkfs"
+SNAPSHOTBASE="replay-base"
+SNAPSHOTCOW="replay-cow"
 
-LOGDEV=$1
-REPLAYDEV=$2
+# Don't modify these
+BLKSIZE=$(blockdev --getsz $REPLAYDEV)
+ORIGIN_TABLE="0 $BLKSIZE snapshot-origin $REPLAYDEV"
+COW_TABLE=
+COW_LOOP_DEV=
+
+_fail()
+{
+	echo $1
+	if [ -n $SNAPDEV ]
+	then
+		dmsetup remove $SNAPSHOTCOW > /dev/null 2>&1
+		dmsetup remove $SNAPSHOTBASE > /dev/null 2>&1
+	fi
+	losetup -d $COW_LOOP_DEV > /dev/null 2>&1
+	exit 1
+}
+
+[ -z $LOGDEV ] && exit "Must set logdev and replaydev"
+[ -z $REPLAYDEV ] && exit "Must set logdev and replaydev"
+
+echo "creating snapshot base"
+echo $ORIGIN_TABLE
+
+# Create a 1tb sparse file
+echo "setting up COW TABLE"
+dd if=/dev/zero of=cow-dev bs=1M count=1 seek=1048576
+COW_LOOP_DEV=$(losetup -f --show cow-dev)
+COW_TABLE="0 $BLKSIZE snapshot /dev/mapper/$SNAPSHOTBASE $COW_LOOP_DEV N 8"
+TARGET=/dev/mapper/$SNAPSHOTCOW
 
 NUM_ENTRIES=$(./replay-log --log $LOGDEV --num-entries)
-ENTRY=2265
-./replay-log --log $LOGDEV --verbose --replay $REPLAYDEV --limit $ENTRY || exit 1
+
+# Yes I know it's confusing, but START_MARK is where we want to start our log
+# replay from, but --start-mark means start the log replay to replay-log,
+# whereas we want to replay up to START_MARK and then carry on from there.
+
+echo "replayin to mark"
+ENTRY=$(./replay-log --log $LOGDEV --find --end-mark $START_MARK)
+./replay-log --log $LOGDEV --replay $REPLAYDEV --limit $ENTRY || exit 1
 let ENTRY+=1
 while [ $ENTRY -lt $NUM_ENTRIES ];
 do
-	./replay-log --limit 1 --log $LOGDEV --replay $REPLAYDEV --start \
-		$ENTRY --verbose || exit 1
-	btrfsck /dev/sda > /dev/null 2>&1 || exit 1
+	echo "replaying entry $ENTRY"
+	./replay-log --limit 1 --log $LOGDEV --replay $REPLAYDEV \
+		--start $ENTRY || _fail "replay failed"
+	dmsetup create $SNAPSHOTBASE --table "$ORIGIN_TABLE"
+	if [ $? -ne 0 ]
+	then
+		# Sometimes this looping is too fast for device-mapper and
+		# we get a random EBUSY, so just sleep for a sec and try
+		# again.
+		sleep 1
+		dmsetup create $SNAPSHOTBASE --table "$ORIGIN_TABLE" || \
+			_fail "Couldn't dmsetup"
+	fi
+	dmsetup create $SNAPSHOTCOW --table "$COW_TABLE" || \
+		_fail "failed to create snapshot"
+	$FSCK $TARGET > /dev/null 2>&1 || _fail "fsck failed at entry $ENTRY"
+	mount $TARGET $TEST_MNT || _fail "mount failed at entry $ENTRY"
+	umount $TEST_MNT
+	$FSCK $TARGET > /dev/null 2>&1 || _fail "fsck failed after mount at " \
+		"entry $ENTRY"
+	dmsetup remove $SNAPSHOTCOW || _fail "failed to remove snapshot"
+	dmsetup remove $SNAPSHOTBASE || _fail "failed to remove base"
 	let ENTRY+=1
 done
